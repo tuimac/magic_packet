939faaca853f5365ee12008b50b35afa2e055e05
Ubuntu <ubuntu@docker.tuimac.private>
2021-11-21 14:19:42 +0000

    done

--- test/test.py
+++ test/test.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 
-def createPacket()
+import socket
+import sys
+
+def createPacket(macaddr):
+    packet = b''
+    for parts in macaddr.split(':'):
+        packet = packet + int(parts, 16).to_bytes(2, 'big')
+    packet = packet + b'\xff' * 96
+    return packet
+
+def sendPacket(packet, ip):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(packet, (ip, 9))
 
 if __name__ == '__main__':
-    createPacket()
-    sendPacket()
-    confirmWakeup()
+    if len(sys.argv) == 1:
+        exit(1)
+    macaddr = sys.argv[1]
+    packet = createPacket(macaddr)
+    sendPacket(packet)
+    #confirmWakeup()
