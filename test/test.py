3b2a0af7f3c731b8a980087aa0ee0060db4c4e12
Ubuntu <ubuntu@docker.tuimac.private>
2021-11-23 14:47:30 +0000

    save

--- test/test.py
+++ test/test.py
@@ -8,12 +8,7 @@ import traceback
 import struct
 
 def createPacket(macaddr):
-    packet = b''
-    macBin = struct.pack('BBBBBB', *([int(octet, 16) for octet in macaddr.split(':')]))
-    packet = b'\xff' * 6
-    packet = packet + macBin * 16
-    print(packet)
-    return packet
+    return b'\xff' * 6 + (struct.pack('BBBBBB', *([int(octet, 16) for octet in macaddr.split(':')]))) * 16
 
 def sendPacket(packet):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -21,13 +16,26 @@ def sendPacket(packet):
     sock.sendto(packet, ('255.255.255.255', 7))
 
 def confirmWakeup():
-    pass
+    def createPacket():
+        ((8 << 8) << 16)
+        # RFC 1071
+
+    def sendpacket(ip, packet):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+        sock.sendto(packet, (ip, 0))
+        return sock.recv(512)
+        
+    packet = createPacket()
+    data = sendPacket('node-master', packet)
+    print(data)
 
 if __name__ == '__main__':
+    '''
     if len(sys.argv) == 1:
         exit(1)
     macaddr = sys.argv[1]
     packet = createPacket(macaddr)
     sendPacket(packet)
-    #confirmWakeup()
+    '''
+    confirmWakeup()
 
