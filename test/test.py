d78f21b9d953672a8615c4c7e443d4955c2fdfa0
Ubuntu <ubuntu@docker.tuimac.private>
2021-11-23 08:02:33 +0000

    testing done

--- test/test.py
+++ test/test.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python3
 
+import time
 import socket
 import sys
+from threading import Thread
+import traceback
+import struct
 
 def createPacket(macaddr):
     packet = b''
-    for parts in macaddr.split(':'):
-        packet = packet + int(parts, 16).to_bytes(2, 'big')
-    packet = packet + b'\xff' * 96
+    macBin = struct.pack('BBBBBB', *([int(octet, 16) for octet in macaddr.split(':')]))
+    packet = b'\xff' * 6
+    packet = packet + macBin * 16
+    print(packet)
     return packet
 
-def sendPacket(packet, ip):
+def sendPacket(packet):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto(packet, (ip, 9))
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sock.sendto(packet, ('255.255.255.255', 7))
+
+def confirmWakeup():
+    pass
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
@@ -21,3 +30,4 @@ if __name__ == '__main__':
     packet = createPacket(macaddr)
     sendPacket(packet)
     #confirmWakeup()
+
