3e8894f7ac299634d16a87b36dd67c4c5d8f90d2
tuimac <tuimac.devadm01@gmail.com>
2022-06-26 12:51:21 +0900

    convert ipaddress to decimal

---
+++ test/arp.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import socket
+import struct
+
+def ipaddr_to_decimal(ipaddr):
+    result = 0
+    octets = ipaddr.split('.')
+    for i in range(0, 4):
+        result += int(octets[i]) << (len(octets) - i - 1) * 8
+    return result
+
+def arp_request(interface, ipaddr):
+    # Create L2 socket
+    sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
+    sock.bind((interface, 0))
+    
+    # Create request datagram
+    packet = struct.pack('!HHBBH')
+
+if __name__ == '__main__':
+    #arp_request('eth0')
+    print(ipaddr_to_decimal('192.168.0.1'))
