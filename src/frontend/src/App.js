bf48def9e75f6aeaf631d52c4e822da212397663
tuimac <tuimac.devadm01@gmail.com>
2022-07-02 13:35:26 +0900

    save

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import HomeLayout from './layouts/HomeLayout';
+import Home from './components/Home';
 
-function App() {
+class App extends React.Component {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+		<>
+      <BrowserRouter>
+        <div>
+          <HomeLayout/>
+          <div>
+            <Switch>
+              <Route component={ Home } path="/" exact />
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/frontend/src/layouts/HomeLayout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class HomeLayout extends React.Component {
+
+  constructor(props) {
+  }
+
+  render() {
+    return(
+      <>
+      </>
+    );
+  };
+}
+
+export default HomeLayout;
