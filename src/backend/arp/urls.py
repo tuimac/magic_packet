38f6e10207ca3a8e197e1205ac87784935c32f56
tuimac <tuimac.devadm01@gmail.com>
2022-08-11 17:15:52 +0900

    save

--- src/backend/arp/urls.py
+++ src/backend/arp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ArpAPIViews.as_view())
+    path('', views.ArpAPIViews.as_view()),
+    path('<str:macaddr>', views.ArpAPIViews.as_view()),
+    path('scan/', views.ArpScanAPIViews.as_view())
 ]
--- src/backend/arp/views.py
+++ src/backend/arp/views.py
@@ -13,14 +13,30 @@ class ArpAPIViews(views.APIView):
 
     def get(self, request, *args, **kwargs):
         try:
+            if self.kwargs.get('macaddr') == None:
+                return Response('{"code": "2", "result": "Need the target IP."}', status=status.HTTP_400_BAD_REQUEST)
             arp = Arp()
             result = dict()
-            if self.kwargs.get('ip') == None:
-                result = arp.scanAll()
+            if self.kwargs.get('macaddr') == None:
+                result = arp.ping()
                 logger.error(result)
             else:
                 result = arp.ping(ip=self.kwargs.get('ip'))
                 logger.error(result)
+            return Response(result, status=status.HTTP_200_OK)
+        except:
+            logger.error(traceback.format_exc())
+            return Response('{"result": "Runtime error."}', status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class ArpScanAPIViews(views.APIView):
+
+    renderer_classes = [JSONRenderer]
+
+    def get(self, request, *args, **kwargs):
+        try:
+            arp = Arp()
+            result = dict()
+            result = arp.scanAll()
             return Response(result, status=status.HTTP_200_OK)
         except:
             logger.error(traceback.format_exc())
